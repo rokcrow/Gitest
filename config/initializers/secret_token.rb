d46a86aab6feb0ccd64e381e9b799f28a78dbd69
rokcrow <rokcrow@gmail.com>
2012-09-16 13:27:14 -0500

    probando git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gitest::Application.config.secret_token = '3a2283bc8979cf2c889aec1aadc3c9b6c0489a4846ce71e5f370f3dc3da2000db9f70d7481898f0872198efdc486d97934bcb617de969dc87f3f109e7bd2c1c3'
